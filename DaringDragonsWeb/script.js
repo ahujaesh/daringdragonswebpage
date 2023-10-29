d34d6b993be698add99faf1d433cf09fafda4bca
ahujaesh <86844958+ahujaesh@users.noreply.github.com>
2023-10-29 11:04:13 -0500

    Add files via upload

---
+++ DaringDragonsWeb/script.js
@@ -0,0 +1,44 @@
+const teamMembers = [
+    { name: "Sahasra K", link: "sahasra_k.html" },
+    { name: "Sahasra V", link: "sahasra_v.html" },
+    { name: "Sristi S", link: "sristi_s.html" },
+    { name: "Gokul S", link: "gokul_s.html" },
+    { name: "Varun I", link: "varun_i.html" },
+    { name: "Eshaan A", link: "eshaan_a.html" },
+    { name: "Ruchika Joshi - The Team Manager", link: "rj.html" }
+];
+
+const searchInput = document.getElementById("search");
+const searchResults = document.getElementById("search-results");
+
+searchInput.addEventListener("input", () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    const filteredMembers = teamMembers.filter(member =>
+        member.name.toLowerCase().includes(searchTerm)
+    );
+
+    displayResults(filteredMembers);
+});
+
+function displayResults(results) {
+    searchResults.innerHTML = "";
+
+    if (results.length === 0) {
+        searchResults.innerHTML = "No results found.";
+        return;
+    }
+
+    results.forEach(member => {
+        const memberLink = document.createElement("a");
+        memberLink.href = member.link;
+        memberLink.textContent = member.name;
+        searchResults.appendChild(memberLink);
+    });
+}
+
+// Clear the results when the search input is empty
+searchInput.addEventListener("input", () => {
+    if (searchInput.value === "") {
+        searchResults.innerHTML = "";
+    }
+});
